20f36f2fa88a00020132f101c805341177121ae8
Brooks J Rady <b.j.rady@gmail.com>
2025-05-01 11:46:32 +0100

    refactor(lib): clean up `queue.rs`
    After moving `ThreadPool` out of `queue.rs`, things can be trimmed down
    and the scope of the module should become clearer.

--- src/queue.rs
+++ src/queue.rs
@@ -1,4 +1,7 @@
-use std::time::{Duration, Instant};
+use std::{
+    sync::Mutex,
+    time::{Duration, Instant},
+};
 
 use color_eyre::eyre::Result;
 
@@ -16,26 +19,35 @@ struct Job {
     status: Status,
 }
 
-struct Queue {
-    jobs: Vec<Job>,
-    thread_pool: WorkerPool,
-    // TODO: If the `Queue` is already running, then any new workflows added to the queue are started immediately!
-    // Otherwise, just add them to `jobs` and wait for `Queue.run()` to be called! This variable should be `true`
-    // as long as anything in the `jobs` list isn't finished (i.e. `Queued` or `Running`)
+pub struct Queue {
+    jobs: Mutex<Vec<Job>>,
+    worker_pool: WorkerPool,
 }
 
 impl Queue {
-    fn new(threads: Option<usize>) -> Result<Self> {
+    pub fn new(workers: usize) -> Result<Self> {
+        let jobs = Mutex::new(Vec::new());
+        let worker_pool = WorkerPool::new(workers)?;
+
+        Ok(Self { jobs, worker_pool })
+    }
+
+    // TODO: If the `Queue` is already running, then any new workflows added to the queue are started immediately!
+    // Otherwise, just add them to `jobs` and wait for `Queue.run()` to be called! This variable should be `true`
+    // as long as anything in the `jobs` list isn't finished (i.e. `Queued` or `Running`)
+    pub fn queue(&mut self) -> Result<()> {
         todo!()
     }
 
-    // TODO: `Self::queue(Workflow)` that just immediately spawns things into the pool, but also adds them to the list
-    // of jobs so that we have a history of what's been queued
-    //
-    // Use `spawn_fifo`?
-
     // TODO: Have a `queue` and `queue_grouped` option, where the `queue` method automatically splits into single-sample
     // `Workflow`s!
+    pub fn queue_grouped(&mut self) -> Result<()> {
+        todo!()
+    }
+
+    pub fn run(&mut self) {
+        todo!()
+    }
 }
 
 #[cfg(test)]
