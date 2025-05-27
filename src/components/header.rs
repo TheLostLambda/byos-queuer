3ab77a961ce3c3ead0123f2990c80603cbd5935d
Brooks J Rady <b.j.rady@gmail.com>
2025-05-27 13:14:39 +0100

    refactor(ui): trim out needless comments and code

--- src/components/header.rs
+++ src/components/header.rs
@@ -4,11 +4,8 @@ use dioxus::prelude::*;
 #[component]
 pub fn Header() -> Element {
     rsx! {
-        // We can create elements inside the rsx macro with the element name followed by a block of attributes and children.
         div {
-            // Attributes should be defined in the element before any children
             class: "flex items-center justify-center gap-8 mb-8",
-            // After all attributes are defined, we can define child elements and components
             img {
                 src: FAVICON,
                 class: "h-24",
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+mod components;
 mod state;
 
 use color_eyre::Result;
-// The dioxus prelude contains a ton of common items used in dioxus apps. It's a good idea to import wherever you
-// need dioxus
 use dioxus::{
     desktop::{self, WindowBuilder},
     prelude::*,
@@ -10,44 +9,14 @@ use dioxus::{
 
 use components::Header;
 
-/// Define a components module that contains all shared components for our app.
-mod components;
-
-// We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
-// The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
 pub const FAVICON: Asset = asset!("/assets/favicon.ico");
-// The asset macro also minifies some assets like CSS and JS to make bundled smaller
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
-// FIXME: Maybe replace this with an `indoc!`
 const INDEX_HTML: &str = include_str!("../index.html");
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    // The `launch` function is the main entry point for a dioxus app. It takes a component and renders it with the platform feature
-    // you have enabled
-    dioxus::LaunchBuilder::new()
-        .with_cfg(
-            desktop::Config::default()
-                .with_menu(None)
-                .with_window(WindowBuilder::new().with_title("Byos Queuer"))
-                .with_custom_index(INDEX_HTML.to_string()),
-        )
-        .launch(App);
-
-    Ok(())
-}
-
-/// App is the main component of our app. Components are the building blocks of dioxus apps. Each component is a function
-/// that takes some props and returns an Element. In this case, App takes no props because it is the root of our app.
-///
-/// Components should be annotated with `#[component]` to support props, better error messages, and autocomplete
 #[component]
 fn App() -> Element {
-    // The `rsx!` macro lets us define HTML inside of rust. It expands to an Element with all of our HTML inside.
     rsx! {
-        // In addition to element and text (which we will see later), rsx can contain other components. In this case,
-        // we are using the `document::Link` component to add a link to our favicon and main CSS file into the head of our app.
         document::Link { rel: "icon", href: FAVICON }
         document::Stylesheet { href: TAILWIND_CSS }
 
@@ -58,29 +27,22 @@ fn App() -> Element {
 
             div {
                 class: "flex flex-col card-body",
-
-                h2 {
-                    class: "card-title",
-                    "Job Queue"
-                }
-
-                input {
-                    r#type: "file",
-                    class: "file-input",
-                    // Select a folder by setting the directory attribute
-                    directory: true,
-                    onchange: move |evt| {
-                        if let Some(file_engine) = evt.files() {
-                            let files = file_engine.files();
-                            for file_name in files {
-                                println!("{file_name}");
-                            }
-                        }
-                    }
-                }
-
-                div { class: "divider" }
             }
         }
     }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    dioxus::LaunchBuilder::new()
+        .with_cfg(
+            desktop::Config::default()
+                .with_menu(None)
+                .with_window(WindowBuilder::new().with_title("Byos Queuer"))
+                .with_custom_index(INDEX_HTML.to_string()),
+        )
+        .launch(App);
+
+    Ok(())
 }
