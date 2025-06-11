335a2b05502f594da727791113bc2158c64bd298
Brooks J Rady <b.j.rady@gmail.com>
2025-06-11 09:54:17 +0100

    style(ui): unify clippy explanation comments

--- src/components/run_button.rs
+++ src/components/run_button.rs
@@ -1,4 +1,4 @@
-// NOTE: The `#[component]` macro is deriving `PartialEq`, but not `Eq` (since that's not needed), but clippy is
+// NOTE: The `#[component]` macro is deriving `PartialEq`, but not `Eq` (since that's not needed), and clippy is
 // complaining about that. This needs to be a module-level `#![expect(...)]` since I can't actually place an
 // `#[expect(...)]` inside of the `#[component]` macro
 #![expect(clippy::derive_partial_eq_without_eq)]
