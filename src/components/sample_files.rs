270aff175e8599c1374a3c913d6e68ada5a490f3
Brooks J Rady <b.j.rady@gmail.com>
2025-06-20 17:29:07 +0100

    feat(ui): fill in tooltip for `SampleFiles`

--- src/components/sample_files.rs
+++ src/components/sample_files.rs
@@ -12,7 +12,7 @@ pub fn SampleFiles(value: Signal<Vec<String>>) -> Element {
     rsx! {
         FilePathPicker {
             label: "Sample Files",
-            tooltip: "***TODO***",
+            tooltip: "Any number of MS data files supported by Byos may be selected",
             value,
             // TODO: Extend this `accept` to allow more than just Thermo RAW files? I'll first have to find out what
             // other file / directory extensions Byos supports...
