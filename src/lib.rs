2fa5f0832decd363fbade9def691adaece68509b
Brooks J Rady <b.j.rady@gmail.com>
2025-06-20 15:19:52 +0100

    style(lib): remove old `TODO` comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-// TODO:
-// 4) Basic (minimally functional) UI for configuring `State`
-// 5) Polish of UI + Skeleton (if time allows!)
-
 pub mod cancellable_timer;
 pub mod job;
 pub mod modifications;
